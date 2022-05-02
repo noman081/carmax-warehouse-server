a82dcfddec8349c0063b0f5313b5a3e4c2765f83
noman081 <abdullahalnoman642@gmail.com>
2022-05-02 16:37:33 +0600

    After connecting db

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -9,9 +10,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('CarMax server is running..............');
-})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kwo2m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        app.get('/', (req, res) => {
+            res.send('CarMax server is running agun..............');
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log('CarMax server is running at port: ', port);
